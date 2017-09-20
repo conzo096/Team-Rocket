a7e0e16c0a490417d5aefbf84a5500c987e24bc7
Conner Weatherston <40167111@live.napier.ac.uk>
2017-09-20 20:47:36 +0100

    Added in model loader.
    Cannot check to see if it properly configured until camera support.

---
+++ AGEproject/src/Model.cpp
@@ -0,0 +1,60 @@
+#include "Model.h"
+#include <sstream>
+Model::Model(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
+{
+	this->vertices = vertices;
+	this->indices = indices;
+	this->textures = textures;
+
+	SetUpMesh();
+}
+
+
+void Model::SetUpMesh()
+{
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
+		&indices[0], GL_STATIC_DRAW);
+
+	// vertex positions
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+	// vertex normals
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+	// vertex texture coords
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, texCoords));
+
+	glBindVertexArray(0);
+}
+
+void Model::Draw(GLShader shader)
+{
+	unsigned int diffuseNr = 1;
+	unsigned int specularNr = 1;
+	for (GLuint i = 0; i < this->textures.size(); i++)
+	{
+		glActiveTexture(GL_TEXTURE0 + i);
+
+		std::string name = this->textures[i].type;
+		std::string number = (name == "texture_diffuse") ? std::to_string(diffuseNr++) : std::to_string(specularNr++);
+
+		glUniform1i(glGetUniformLocation(shader.GetId(), ("material." + name + number).c_str()), i);
+		glBindTexture(GL_TEXTURE_2D, this->textures[i].id);
+	}
+	glActiveTexture(GL_TEXTURE0);
+
+	// draw mesh
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+}
---
+++ AGEproject/src/Model.h
@@ -0,0 +1,116 @@
+#pragma once
+#include <glm\glm.hpp>
+#include <glm\gtc\quaternion.hpp> 
+#include <string>
+#include <vector>
+#include "GLShader.h"
+#include "assimp/Importer.hpp"	//OO version Header!
+#include "assimp/PostProcess.h"
+#include "assimp/Scene.h"
+#include <math.h>
+#include <fstream>
+#include <map>
+
+struct Transform
+{
+	glm::vec3 scale;
+	glm::quat orientation;
+	glm::vec3 position;
+};
+
+struct Vertex
+{
+	glm::vec3 position;
+	glm::vec3 normal;
+	glm::vec2 texCoords;
+};
+
+struct Texture
+{
+	unsigned int id;
+	std::string type;
+};
+
+class Model
+{
+private:
+	unsigned int VAO, VBO, EBO;
+	void SetUpMesh();
+
+
+
+public:
+	Model(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+
+	Transform transform;
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+
+
+	void Draw(GLShader shader);
+
+	Model(const std::string& fileName)
+	{
+		// Create model importer
+		Assimp::Importer loadModel;
+		// Read in the model data 
+		const aiScene *model = loadModel.ReadFile(fileName, aiProcess_Triangulate
+			| aiProcess_GenSmoothNormals
+			| aiProcess_ValidateDataStructure
+			| aiProcess_FindInvalidData
+			| aiProcess_FixInfacingNormals
+			| aiProcess_ImproveCacheLocality
+			| aiProcess_GenUVCoords);
+		// Check that data has been read in correctly
+		if (!model)
+		{
+			// Display error
+			std::fprintf(stderr,"Data incorrectly read in at $s",fileName);
+			std::fprintf(stderr, "$s", loadModel.GetErrorString());
+			// Throw exception
+			throw std::runtime_error("Error reading in model file");
+		}
+		// Vectors to store model data.
+	
+		unsigned int vertex_begin = 0;
+		// Loop throw each sub-mesh.
+		for (unsigned int i = 0; i < model->mNumMeshes; i++)
+		{
+			// Get the sub-mesh.
+			aiMesh *modelMesh = model->mMeshes[i];
+			// get the vertex positions.
+			
+			for (unsigned int j = 0; j < modelMesh->mNumVertices; j++)
+			{
+				Vertex vertex;
+				aiVector3D pos = modelMesh->mVertices[j];
+				vertex.position = glm::vec3(pos.x, pos.y, pos.z);
+				aiVector3D norm = modelMesh->mNormals[j];
+				vertex.normal = glm::vec3(norm.x, norm.y, norm.z);
+				/*aiColor4D colour = modelMesh->mColors[0][j];
+				colours.push_back(glm::vec4(colour.r, colour.g, colour.b, colour.a));*/
+				auto tex_coord = modelMesh->mTextureCoords[0][j];
+				vertex.texCoords = glm::vec2(tex_coord.x, tex_coord.y);
+				vertices.push_back(vertex);
+			}
+
+			// If we have face information, then add to index buffer
+			if (modelMesh->HasFaces() == true)
+			{
+				for (unsigned int j = 0; j < modelMesh->mNumFaces; j++)
+				{
+					aiFace modelFace = modelMesh->mFaces[j];
+					for (unsigned int l = 0; l < 3; l++)
+						indices.push_back(vertex_begin + modelFace.mIndices[l]);
+				}
+			}
+			vertex_begin += modelMesh->mNumVertices;
+
+
+		}
+
+		SetUpMesh();
+	}
+
+};
