0de3f38d4fa2a197827ea899e85d102baa9f18b5
Jack Yorston <40174030@live.napier.ac.uk>
2017-11-03 15:04:46 +0000

    SettingsMenu label/button layout complete

--- AGEproject/src/SettingsMenu.cpp
+++ AGEproject/src/SettingsMenu.cpp
@@ -8,17 +8,66 @@ int SettingsMenu::Draw(GLShader shader)
 {
 	const int numOfLabels = numOfTitles + numOfOptions;
 	const int numOfButtons = numOfSmallButtons + numOfLargeButtons;
+
 	labels.resize(numOfLabels);
 	buttons.resize(numOfButtons);
 	
-	// Render titles
+	// Render title labels
 	for (int i = 0; i < numOfTitles; i++)
 	{
 		Label& newTitle = labels[i];
 		newTitle.texture = tex;
-		newTitle.renderTarget = Quad(glm::vec2(0 - (titleWidth / 2.0f), 0 - (titleHeight / 2.0f)),
-									 glm::vec2(0 + (titleWidth / 2.0f), 0 + (titleHeight / 2.0f)));
+		newTitle.renderTarget = Quad(glm::vec2(titleOffsetX - 1, 1 - titleOffsetY - titleHeight),
+									 glm::vec2(titleOffsetX - 1 + titleWidth, 1 - titleOffsetY));
 		newTitle.renderTarget.SetOpenGL();
+		buttonOffsetY_Large += titleOffsetY;
+		titleOffsetY += (titleHeight + changeOffset_Title);
+	}
+
+	// Render option labels
+	for (int i = 0; i < numOfOptions; i++)
+	{
+		Label& newOption = labels[i + numOfTitles];
+		newOption.texture = tex;
+		newOption.renderTarget = Quad(glm::vec2(1 - optionOffsetX - optionWidth, 1 - optionOffsetY - optionHeight),
+									  glm::vec2(1 - optionOffsetX, 1 - optionOffsetY));
+		newOption.renderTarget.SetOpenGL();
+		optionOffsetY += (optionHeight + changeOffset_Option);
+	}
+	
+	// Render large buttons
+	for (int i = 0; i < numOfLargeButtons; i++)
+	{
+		Button& newLargeButton = buttons[i];
+		newLargeButton.texture = tex;
+		newLargeButton.action = i;
+		// Create a wider button on the first loop
+		if (i == 0)
+			buttonWidth_Large = 0.8f;
+		else
+			buttonWidth_Large = 0.6f;
+		newLargeButton.renderTarget = Quad(glm::vec2(0 - (buttonWidth_Large / 2.0f), 1 - buttonOffsetY_Large - buttonHeight_Large),
+									       glm::vec2(0 + (buttonWidth_Large / 2.0f), 1 - buttonOffsetY_Large));
+		newLargeButton.renderTarget.SetOpenGL();
+		buttonOffsetY_Large += (changeOffset_Button_Large + buttonHeight_Large);
+	}
+
+	// Render small buttons
+	for (int i = 0; i < numOfSmallButtons; i++)
+	{
+		Button& newSmallButton = buttons[i + numOfLargeButtons];
+		newSmallButton.texture = tex;
+		newSmallButton.action = i;
+		newSmallButton.renderTarget = Quad(glm::vec2(1 - buttonOffsetX_Small - buttonWidth_Small, 1 - buttonOffsetY_Small - buttonHeight_Small),
+										   glm::vec2(1 - buttonOffsetX_Small, 1 - buttonOffsetY_Small));
+		newSmallButton.renderTarget.SetOpenGL();
+		if (i % 2 != 0)
+		{
+			buttonOffsetX_Small += (buttonWidth_Small + changeOffset_ButtonX_Small);
+			buttonOffsetY_Small -= (buttonHeight_Small + changeOffset_ButtonY_Small);
+		}
+		else
+			buttonOffsetY_Small += (buttonHeight_Small + changeOffset_ButtonY_Small);
 	}
 
 	while (!selectionMade)
@@ -40,14 +89,14 @@ int SettingsMenu::Draw(GLShader shader)
 			glBindTexture(GL_TEXTURE_2D, labels.at(i).texture);
 			labels[i].renderTarget.Draw();
 		}
-		//for (int i = 0; i < numOfButtons; i++)
-		//{
-		//	// Bind texture.
-		//	glUniform1i(shader.GetUniformLocation("tex"), 0);
-		//	glActiveTexture(GL_TEXTURE0);
-		//	glBindTexture(GL_TEXTURE_2D, buttons.at(i).texture);
-		//	buttons[i].renderTarget.Draw();
-		//}
+		for (int i = 0; i < numOfButtons; i++)
+		{
+			// Bind texture.
+			glUniform1i(shader.GetUniformLocation("tex"), 0);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, buttons.at(i).texture);
+			buttons[i].renderTarget.Draw();
+		}
 		glfwSwapBuffers(GameEngine::Get().GetWindow());
 
 		if (glfwWindowShouldClose(GameEngine::Get().GetWindow()))
--- AGEproject/src/SettingsMenu.h
+++ AGEproject/src/SettingsMenu.h
@@ -4,7 +4,36 @@
 class SettingsMenu : public Menu
 {
 public:
-	SettingsMenu() {}
+	SettingsMenu()
+	{
+		numOfTitles = 2;
+		numOfOptions = 2;
+		numOfLargeButtons = 3;
+		numOfSmallButtons = 4;
+
+		titleWidth = 0.5f;
+		titleHeight = 0.18f;
+		optionWidth = 0.3f;
+		optionHeight = 0.15f;
+		buttonHeight_Large = titleHeight;
+		buttonWidth_Small = 0.03f;
+		buttonHeight_Small = 0.05f;
+
+		titleOffsetX = 0.5f;
+		titleOffsetY = 0.33f;
+		optionOffsetX = 0.55f;
+		optionOffsetY = titleOffsetY + 0.015f;
+		buttonOffsetX_Large = 0.25f;
+		buttonOffsetY_Large = 0.0f;
+		buttonOffsetX_Small = 0.5f;
+		buttonOffsetY_Small = optionOffsetY + (optionHeight / 2.0f) - (buttonHeight_Small / 2.0f);
+
+		changeOffset_Title = 0.11f;
+		changeOffset_Option = 0.14f;
+		changeOffset_Button_Large = changeOffset_Title;
+		changeOffset_ButtonX_Small = 0.0395f + optionWidth;
+		changeOffset_ButtonY_Small = (titleHeight / 2.0f) + changeOffset_Title + (optionHeight / 2.0f) - (buttonHeight_Small / 2.0f) - 0.01f;
+	}
 	~SettingsMenu() {}
 
 	std::vector<Label> labels;
@@ -22,22 +51,22 @@ public:
 private:
 	unsigned int tex = Texture("../res/textures/debug.png").GetTextureId(); //Texture("../res/textures/MainMenu_Button1.png").GetTextureId(); 
 
-	const int numOfTitles = 1;
-	const int numOfOptions = 0;
-	const int numOfSmallButtons = 0;
-	const int numOfLargeButtons = 0;
+	int numOfTitles;
+	int numOfOptions;
+	int numOfSmallButtons;
+	int numOfLargeButtons;
 
-	const float titleWidth = 1.0f;
-	const float titleHeight = 0.6f;
-	const float optionWidth = 0;
-	const float optionHeight = 0;
-	const float buttonWidth_Small = 0;
-	const float buttonHeight_Small = 0;
-	const float buttonWidth_Large = 0;
-	const float buttonHeight_Large = 0;
+	float titleWidth;
+	float titleHeight;
+	float optionWidth;
+	float optionHeight;
+	float buttonWidth_Small;
+	float buttonHeight_Small;
+	float buttonWidth_Large;
+	float buttonHeight_Large;
 
-	float titleOffsetX = 1.0f;
-	float titleOffsetY = 0.8f;
+	float titleOffsetX;
+	float titleOffsetY;
 	float optionOffsetX;
 	float optionOffsetY;
 	float buttonOffsetX_Small;
@@ -45,10 +74,9 @@ private:
 	float buttonOffsetX_Large;
 	float buttonOffsetY_Large;
 
-	const float changeOffset_Title = 0;
-	const float changeOffset_Option = 0;
-	const float changeOffset_ButtonX_Small = 0;
-	const float changeOffset_ButtonY_Small = 0;
-	const float changeOffset_ButtonX_Large = 0;
-	const float changeOffset_ButtonY_Large = 0;
+	float changeOffset_Title;
+	float changeOffset_Option;
+	float changeOffset_ButtonX_Small;
+	float changeOffset_ButtonY_Small;
+	float changeOffset_Button_Large;
 };
