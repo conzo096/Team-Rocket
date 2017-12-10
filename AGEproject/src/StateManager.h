30b1a2679248febac6eddf597e007daff2647e3c
Matthew Jenkinson <matthew.j.jenkinson@gmail.com>
2017-12-10 06:33:16 +0000

    Splash screen in
    worked first time, I don't believe it

--- AGEproject/src/StateManager.h
+++ AGEproject/src/StateManager.h
@@ -8,6 +8,10 @@
 #include "MainMenu.h"
 #include "SettingsMenu.h"
 #include "ControlsMenu.h"
+#include <thread>
+#include <chrono>
+
+
 class StateManager : public Singleton<StateManager>
 {
 	enum State
@@ -47,6 +51,31 @@ public:
 
 	int ShowSplashScreen()
 	{
+		// Initialise required assets, time, shader, quad, texture
+		const std::chrono::milliseconds timeToWait(5000); // 5 Seconds
+		GLShader shader = *ResourceHandler::Get().GetShader("Basic");
+		Quad quad = Quad();
+		const unsigned int tex = ResourceHandler::Get().GetTexture("Splash_Screen");
+
+		// Do OpenGL & shader things
+		quad.SetOpenGL();
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+		glClearColor(0, 0, 1, 1);
+		shader.Use();
+
+		// Do texture & quad things
+		glBindTexture(GL_TEXTURE_2D, tex);
+		quad.Draw();
+
+		// Render it
+		glfwSwapBuffers(GameEngine::Get().GetWindow());
+		glfwPollEvents();
+
+		// After drawing, start waiting
+		std::this_thread::sleep_for(timeToWait);
+
+		// Show the main menu
 		return stateMainMenu;
 	}
 
